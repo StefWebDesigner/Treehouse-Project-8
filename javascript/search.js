c1f72a60853a911523f397dbac985611fee64db5
StefWebDesigner <67237726+StefWebDesigner@users.noreply.github.com>
2021-07-07 13:37:51 -0400

    the filter just got the green light!

--- javascript/search.js
+++ javascript/search.js
@@ -4,46 +4,48 @@ const search = document.querySelector('#search');
 // const feild_info = document.querySelectorAll('.card p');
 //IT works but takes everything out
 // const feild_info = document.querySelectorAll('main');
-const feild_info = document.querySelector('.text-container p');
-
-
-//IT could be that all are celebted and for this reason it all goes away
 
 
 //SEARCH EVENT FUNCTION 
 const handleSearch = event => {
-
-    console.log('Entering the function');
-
-    search.addEventListener('keyup', handleSearch);
+    //make it show the .name somewhere
+    // const feild_info = document.querySelectorAll('.text-container p');
+    const feild_info = document.querySelectorAll('h2.name');
     const searchTerm = event.target.value.toLowerCase();
 
-    console.log('Listerner activated');
-
+    //TEST 
+    console.log('Entering the function');
     
+    //FOREACH LOOP
     feild_info.forEach( feild_var => {
 
+        //TEST
         console.log('It has enteredd the forEach loop');
 
         const text = feild_var.textContent.toLowerCase();
-        //this must be the issue
-        const box = feild_var.parentElement;
-        
+        //Need to go up to two parent elements
+        // const box = feild_var.parentElement.parentElement; 
+        const box = feild_var.parentElement.parentElement;        
+       
 
         if (text.includes(searchTerm)) {
             box.style.display = "block";
-            //attach the card
 
+            //TEST
             console.log('In blocked');
 
         } else {
             box.style.display = "none";
             
+            //TEST
             console.log('In none');
-
         }
-
     });
 }
 
+
 search.addEventListener('keyup', handleSearch);
+console.log('Listerner activated');
+
+
+        //NEed to think about the filter will look after it is filtered though!!!!! --- Extra Credit!
